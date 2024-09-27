21cdedae1ab896ace24ee1c9002a401b39a362ae
Anonymous <tmp@tmp.com>
2024-09-27 15:08:56 +0900

    Comment: Added comment

--- icestore.js
+++ icestore.js
@@ -27,8 +27,11 @@ let flavorDistribution = transactions.reduce((acc, curr) => {
     }, {}) // { Chocolate: 1, Vanilla: 3, Mint Chip: 1, Raspberry: 1, StrawBerry: 1 }
 console.log(flavorDistribution);
 
+//최대값
 let most = -1;
+//최대값의 이름
 let mostPick = "Unknown";
+//키를 뽑아내어 flavorDistribution에 순차접근. 현재 최대값보다 이번 값이 크면 최대값과 그 이름을 갱신.
 Object.keys(flavorDistribution).forEach((value) => {
     if(flavorDistribution[value] > most) {
         most = flavorDistribution[value];
@@ -36,4 +39,5 @@ Object.keys(flavorDistribution).forEach((value) => {
     }
 })
 
+//최대값 이름 출력
 console.log(mostPick);
