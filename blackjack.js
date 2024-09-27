fc5d247faca4d64e44ebaa42b9c9ef0883f42d9b
Anonymous <tmp@tmp.com>
2024-09-27 14:25:26 +0900

    Added: Draw decision logic Added: Dealer draws new card until reach 17+ Changed: Player will win first 2 cards is blackjack

--- blackjack.js
+++ blackjack.js
@@ -1,22 +1,47 @@
-let cardOne = 7;
-let cardTwo = 5;
-let sum = cardOne + cardTwo;
-let cardOneBank = 7;
-let cardTwoBank = 5;
-let cardThreeBank = 6;
-let cardFourBank = 4;
+let card1 = 7;
+let card2 = 5;
+let card3 = 9;
+let sum = card1 + card2;
 
-let cardThree = 7;
-sum += cardThree;
-if(sum > 21) {
-    console.log('You Lost');
+//딜러가 뽑게될 카드들
+let banks = [7, 5, 6, 4];
+
+//게임 종료 시 true
+function checkGameSet() {
+    if(sum === 21) {
+        console.log("BLACKJACK");
+        return true;
+    }
+    else if(sum > 21) {
+        console.log('BUST');
+        return true;
+    }
+    return false;
+}
+
+//카드 2개로 게임 종료 체크. 끝나지 않았다면, 한 장 더 뽑아본다.
+if(!checkGameSet()) {
+    sum += card3;
+    checkGameSet();
 }
 console.log(`You have ${sum} points`);
 
-let bankSum = cardOneBank + cardTwoBank + cardThreeBank + cardFourBank;
+//딜러가 카드를 뽑을 차례. 17 미만이면 계속 뽑아본다.
+let bankSum = 0;
+let i = 0;
+while(bankSum < 17) {
+    bankSum += banks[i++];
+}
 
-if(bankSum > 21 || sum <=21 && sum >bankSum) {
+//딜러가 버스트 or 플레이어가 딜러보다 높음 = 승리
+//같은 점수 = 무승부
+//그 외 = 패배
+if(bankSum > 21 || (sum <= 21 && sum > bankSum)) {
     console.log("You win");
-}else {
-    console.log("bank wins");
+}
+else if(bankSum === sum) {
+    console.log("Draw");
+}
+else {
+    console.log("Bank wins");
 }
