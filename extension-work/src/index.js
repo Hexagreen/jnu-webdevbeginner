dc2cb802e594fd70e539f19b5a8a53d1469a909e
Hexagreen <greentree329@naver.com>
2024-11-17 07:52:16 +0900

    Feature: Day Night toggle button

--- extension-work/src/index.js
+++ extension-work/src/index.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 const hTitle = document.querySelector("#title");
-const hSubtitle = document.querySelector("#subtitle")
+const hSubtitle = document.querySelector("#subtitle");
 const divLoading = document.querySelector(".loading");
 const divResult = document.querySelector(".result");
+const modeButton = document.querySelector("#modeButton");
+const dayNight = { day: false };
 
 class Coordinate {
     constructor(x, y) {
@@ -13,19 +15,20 @@ class Coordinate {
 }
 
 class Weather {
-    constructor(forecast) {
+    constructor(forecast, targetTime = 24) {
         const date = new Date();
         const nowLocalTime = date.getHours();
-        const hoursToMidnight = 24 - nowLocalTime;
-        const midnightInUTC = date.getUTCHours() + hoursToMidnight;
+        let hoursToTargetTime = targetTime - nowLocalTime;
+        if (hoursToTargetTime < 0) hoursToTargetTime += 24;
+        const targetTimeInUTC = date.getUTCHours() + hoursToTargetTime;
         const initTime = parseInt(forecast.init) % 100;
-        let initTimeToMidnightInUTC = midnightInUTC - initTime;
-        initTimeToMidnightInUTC = initTimeToMidnightInUTC === 0 ? 24 : initTimeToMidnightInUTC;
-        let timepointIndex = Math.round(initTimeToMidnightInUTC / 3.0) - 1;
+        let initTimeToTargetTimeInUTC = targetTimeInUTC - initTime;
+        initTimeToTargetTimeInUTC = initTimeToTargetTimeInUTC === 0 ? 24 : initTimeToTargetTimeInUTC;
+        let timepointIndex = Math.round(initTimeToTargetTimeInUTC / 3.0) - 1;
         if (timepointIndex < 0) timepointIndex = 0;
 
         const target = forecast.dataseries[timepointIndex];
-        this.timepoint = target.timepoint + initTime - (date.getTimezoneOffset / 60);
+        this.timepoint = (target.timepoint + initTime - (date.getTimezoneOffset() / 60)) % 24;
         this.cloudLevel = getCloudLevel();      //0: completly clear 1: little bit 2: cloudy 3: filled
         this.starLevel = getStarLevel();        //0: faint star 1: dark star 2: bright star 3: brightest star
         this.transparency = getTransparency();  //0: clear 1: thin 2: thick 3: blocked
@@ -37,7 +40,7 @@ class Weather {
         this.temperature = target.temp2m;       //celcius
 
         function getCloudLevel() {
-            switch (target.timepoint) {
+            switch (target.cloudcover) {
                 case 1: return 0;
                 case 2:
                 case 3:
@@ -77,7 +80,7 @@ class Weather {
         }
 
         function getInstability() {
-            switch (target.seeing) {
+            switch (target.lifted_index) {
                 case -10:
                 case -6: return 3;
                 case -4: return 2;
@@ -138,6 +141,12 @@ function setSubtitle(message) {
     hSubtitle.innerText = message;
 }
 
+function setBackImage(image, alt) {
+    const img = document.querySelector("#mainImage");
+    img.alt = alt;
+    img.src = `images/${image}.png`;
+}
+
 function setLoadingMessage(message) {
     const info = divLoading.querySelector("#loadInfo");
     info.innerText = message;
@@ -149,7 +158,7 @@ function setLoadStatFailed() {
 }
 
 function isNeedRefresh() {
-    if(localStorage.getItem('weather')) return true;
+    if (localStorage.getItem('weather') === null) return true;
     const lastTime = localStorage.getItem('baseTime');
     const now = new Date();
     const nowTime = '' + now.getUTCFullYear() + (now.getUTCMonth() + 1) + now.getUTCDate() + now.getUTCHours();
@@ -160,32 +169,14 @@ function isNeedRefresh() {
     return false;
 }
 
-async function getIP() {
-    setLoadingMessage("IP 주소 확인 중...")
-    try {
-        await axios
-            .get('https://api.myip.com')
-            .then((response) => {
-                return response.data.ip;
-            });
-    } catch (error) {
-        setLoadingMessage("외부 IP주소를 받아오지 못했습니다.");
-        setLoadStatFailed();
-        return null;
-    }
-}
-
-async function getCoord(ip) {
+async function getCoord() {
     setLoadingMessage("위치 정보 파악 중...")
     try {
-        await axios
-            .get(`http://ip-api.com/json/${ip}`)
-            .then((response) => {
-                return new Coordinate(response.data.lat, response.data.lon)
-            });
+        const response = await axios.get('http://ip-api.com/json/112.164.229.46');
+        return new Coordinate(response.data.lat, response.data.lon);
     } catch (error) {
-        setLoadingMessage("IP 기반 위치정보 파악에 실패했습니다.");
         setLoadStatFailed();
+        setLoadingMessage("IP 기반 위치정보 파악에 실패했습니다.");
         return null;
     }
 }
@@ -193,7 +184,7 @@ async function getCoord(ip) {
 async function getWeather(coord) {
     setLoadingMessage("날씨 정보 불러오는 중...")
     try {
-        await axios
+        const response = await axios
             .get('http://www.7timer.info/bin/api.pl', {
                 params: {
                     lon: coord.longitude,
@@ -201,13 +192,11 @@ async function getWeather(coord) {
                     product: 'astro',
                     output: 'json'
                 }
-            })
-            .then((response) => {
-                return response.data;
             });
+        return response.data;
     } catch (error) {
-        setLoadingMessage("날씨 정보를 받아오지 못했습니다.");
         setLoadStatFailed();
+        setLoadingMessage("날씨 정보를 받아오지 못했습니다.");
         return null;
     }
 }
@@ -219,55 +208,68 @@ function setGridElement(id, imgName, status, desc) {
     <p class='weatherDesc'>${desc}</p>`;
 }
 
+function setGridElemLoaded(id) {
+    divResult.querySelector(id).classList.add("elemLoaded");
+}
+
+function resetGridElemLoaded() {
+    divResult.querySelectorAll(".elemLoaded").forEach((t) => t.classList.remove("elemLoaded"));
+}
+
 function displayCloudLevel(cLevel) {
     const id = "#resCloud";
-    switch(cLevel) {
+    switch (cLevel) {
         case 0: setGridElement(id, 'moon', '청명', '구름 한 점 없어요'); break;
         case 1: setGridElement(id, 'moon', '구름 조금', '맑은 하늘이에요'); break;
         case 2: setGridElement(id, 'moon with cloud', '구름 많음', '구름이 꼈어요'); break;
         case 3: setGridElement(id, 'clouds', '구름 가득', '온통 구름이에요'); break;
     }
+    setGridElemLoaded(id);
 }
 
 function displayStarLevel(sLevel) {
     const id = "#resSeeing";
-    switch(sLevel) {
+    switch (sLevel) {
         case 0: setGridElement(id, 'faint star', '가득한 별', '잘 안보이던 별도 보일 거예요'); break;
         case 1: setGridElement(id, 'dark star', '많은 별', '어두운 별도 보일 거예요'); break;
         case 2: setGridElement(id, 'bright star', '밝은 별만', '어두운 별은 안 보여요'); break;
         case 3: setGridElement(id, 'bright star', '낮은 시계', '밝은 별만 보일 거예요'); break;
     }
+    setGridElemLoaded(id);
 }
 
 function displayTransparency(tLevel) {
     const id = "#resTransparency";
     const img = "solar ray penetrate cloud";
-    switch(tLevel) {
+    switch (tLevel) {
         case 0: setGridElement(id, img, '투명한 하늘', '하늘이 선명히 보여요'); break;
         case 1: setGridElement(id, img, '맑은 하늘', '투과율이 높아요'); break;
         case 2: setGridElement(id, img, '흐릿한 하늘', '대기가 흔들릴 거예요'); break;
         case 3: setGridElement(id, img, '차단됨', '빛이 차단됐어요'); break;
     }
+    setGridElemLoaded(id);
 }
 
 function displayPrecipitation(falling) {
     const id = "#resRain";
-    switch(falling) {
+    switch (falling) {
         case 'rain': setGridElement(id, 'rainy', '비', '비가 내릴 거예요'); break;
         case 'snow': setGridElement(id, 'snowy', '눈', '눈이 내릴 거예요'); break;
         case 'none': setGridElement(id, 'moon with cloud', '강수 없음', '비는 오지 않아요'); break;
     }
+    setGridElemLoaded(id);
 }
 
 function displayInstability(iLevel) {
     const id = "#resInstability";
     const img = "thunderbolt"
-    switch(iLevel) {
-        case 0: setGridElement(id, img, '안정', '대기가 안정적이에요'); break;
-        case 1: setGridElement(id, img, '우르릉..', '대기가 조금 불안정해요'); break;
-        case 2: setGridElement(id, img, '불안정', '대기가 불안정해요'); break;
-        case 3: setGridElement(id, img, '섬광폭풍', '번개가 잔뜩 칠 거예요'); break;
+    switch (iLevel) {
+        case 0: setGridElement(id, img, '안정', '대기가\n안정적이에요'); break;
+        case 1: setGridElement(id, img, '우르릉..', '대기가 조금\n불안정해요'); break;
+        case 2: setGridElement(id, img, '불안정', '대기가\n불안정해요'); break;
+        case 3: setGridElement(id, img, '섬광폭풍', '번개가\n잔뜩 칠 거예요'); break;
     }
+    setGridElemLoaded(id);
 }
 
 function displayHumidWind(humid, wSpeed, wDir) {
@@ -275,57 +277,89 @@ function displayHumidWind(humid, wSpeed, wDir) {
     const img = "humidity and wind"
     let humidInfo;
     let windInfo;
-    switch(humid) {
+    switch (humid) {
         case 20: humidInfo = '바싹 마름'; break;
         case 100: humidInfo = '물 속'; break;
         default: humidInfo = humid + '%'; break;
     }
-    switch(wSpeed) {
+    switch (wSpeed) {
         case 0: windInfo = '무풍'; break;
         case 35: windInfo = '폭풍'; break;
         default: windInfo = `약 ${wSpeed}m/s ${wDir}`; break;
     }
 
-    setGridElement(id, img, '습도와 바람', `${humidInfo} / ${windInfo}`);
+    setGridElement(id, img, '상대습도 / 바람', `${humidInfo} / ${windInfo}`);
+    setGridElemLoaded(id);
 }
 
-function displayTemperature(sLevel) {
-    const id = "#resSeeing";
-    switch(sLevel) {
-        case 0: setGridElement(id, );
-        case 1: setGridElement(id, );
-        case 2: setGridElement(id, );
-        case 3: setGridElement(id, );
+async function displayWeather() {
+    if (isNeedRefresh()) {
+        const coord = await getCoord();
+        if (!coord) return;
+        let weatherFull = await getWeather(coord);
+        if (!weatherFull) return;
+        localStorage.setItem('weather', JSON.stringify(weatherFull));
     }
-}
-
-function init() {
     let weather;
-    if(isNeedRefresh()) {
-        const ip = getIP();
-        if (ip === null) return;
-        const coord = getCoord(ip);
-        if (coord === null) return;
-        const astro = getWeather(coord);
-        weather = new Weather(astro);
-        localStorage.setItem('weather', JSON.stringify(weather));
-    }
-    else {
-        weather = JSON.parse(localStorage.getItem('weather'));
-    }
+    if (dayNight.day) weather = new Weather(JSON.parse(localStorage.getItem('weather')), 12);
+    else weather = new Weather(JSON.parse(localStorage.getItem('weather')));
 
     divLoading.style.display = 'none';
     divResult.style.display = 'grid';
 
     setSubtitle(`내일 ${weather.timepoint}시 기준\n기온 ${weather.temperature}℃`);
-    displayCloudLevel(weather.cloudLevel);
-    displayStarLevel(weather.starLevel);
-    displayTransparency(weather.transparency);
-    displayPrecipitation(weather.prec);
-    displayInstability(weather.instability);
-    displayHumidWind(weather.humidity, weather.windPower, weather.windDirection);
+    setTimeout(() => { displayCloudLevel(weather.cloudLevel) }, 100);
+    setTimeout(() => { displayStarLevel(weather.starLevel) }, 200);
+    setTimeout(() => { displayTransparency(weather.transparency) }, 300);
+    setTimeout(() => { displayPrecipitation(weather.prec) }, 400);
+    setTimeout(() => { displayInstability(weather.instability) }, 500);
+    setTimeout(() => { displayHumidWind(weather.humidity, weather.windPower, weather.windDirection) }, 600);
 }
 
-divResult.style.display = 'none';
-setLoadingMessage('불러오는 중...');
+function toggleMode(e) {
+    e.preventDefault();
+    dayNight.day = !dayNight.day;
+    if (dayNight.day) {
+        setTitle("Radiant Skies");
+        setSubtitle("오늘 날씨가 좋을까요?");
+        setBackImage("DALLE day", "blue sky background");
+        document.body.classList.remove("night");
+        [...document.getElementsByTagName("div")].forEach(d => d.classList.remove("night"));
+        document.querySelectorAll(".weatherDesc").forEach(d => d.classList.remove("night"));
+        resetGridElemLoaded();
+        displayWeather();
+        document.querySelectorAll("#modeButton").forEach(d => d.classList.remove("night"));
+        document.querySelectorAll("#loadStatus.onLoading").forEach(d => d.classList.remove("night"));
+    }
+    else {
+        setTitle("Starry Night");
+        setSubtitle("오늘의 밤하늘은 어떨까요?");
+        setBackImage("DALLE star2", "starry night background");
+        document.body.classList.add("night");
+        [...document.getElementsByTagName("div")].forEach(d => d.classList.add("night"));
+        document.querySelectorAll(".weatherDesc").forEach(d => d.classList.add("night"));
+        resetGridElemLoaded();
+        displayWeather();
+        document.querySelectorAll("#modeButton").forEach(d => d.classList.add("night"));
+        document.querySelectorAll("#loadStatus.onLoading").forEach(d => d.classList.add("night"));
+    }
+}
+
+function init() {
+    divResult.style.display = 'none';
+    setLoadingMessage('불러오는 중...');
+    setTitle("Starry Night");
+    setSubtitle("오늘의 밤하늘은 어떨까요?");
+    setBackImage("DALLE star2", "starry night background");
+    document.body.classList.add("night");
+    [...document.getElementsByTagName("div")].forEach(d => d.classList.add("night"));
+    document.querySelectorAll(".weatherDesc").forEach(d => d.classList.add("night"));
+    resetGridElemLoaded();
+    displayWeather();
+    document.querySelectorAll("#modeButton").forEach(d => d.classList.add("night"));
+    document.querySelectorAll("#loadStatus.onLoading").forEach(d => d.classList.add("night"));
+    modeButton.addEventListener('click', (e) => toggleMode(e));
+}
+
 init();
+displayWeather();
